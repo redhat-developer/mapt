e7768ae7a2c1f59f13dd23fa38ad0dfd099554bd
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-09-26 16:59:17 +0200

    added image for run qenvs

---
+++ images/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+VALID_CONFIG=true
+# Check required ENVs
+if [ -z "${INSTANCE_TYPE}" ]; then 
+  echo "INSTANCE_TYPE is required"
+  VALID_CONFIG=false
+fi
+
+if [ -z "${PRODUCT_DESCRIPTION}" ]; then 
+  echo "PRODUCT_DESCRIPTION ENV is required"
+  VALID_CONFIG=false  
+fi
+
+if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ] || [ -z "${AWS_DEFAULT_REGION}" ]; then 
+  echo "AWS ENV for credentials are required"
+  VALID_CONFIG=false  
+fi
+
+if [ -z "${PULUMI_CONFIG_PASSPHRASE}" ]; then 
+  echo "PULUMI_CONFIG_PASSPHRASE ENV is required"
+  VALID_CONFIG=false  
+fi
+
+if [ "${VALID_CONFIG}" = false ]; then
+  echo "Add the required ENVs"
+  exit 1
+fi
+
+# Run qenvs
+exec qenvs spot \
+    --instance-type "${INSTANCE_TYPE}" \
+    --product-description "${PRODUCT_DESCRIPTION}"
