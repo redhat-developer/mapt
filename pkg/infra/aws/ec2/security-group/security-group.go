7ccd1e565c96f280720fcaed8851f0a45da34ad5
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-10-06 14:54:33 +0200

    added security group pkg on aws ec2 with basic group creation

---
+++ pkg/infra/aws/ec2/security-group/security-group.go
@@ -0,0 +1,64 @@
+package securitygroup
+
+import (
+	"fmt"
+
+	"github.com/adrianriobo/qenvs/pkg/infra"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type IngressRules struct {
+	Description string
+	FromPort    int
+	ToPort      int
+	Protocol    string
+}
+
+type SGRequest struct {
+	Name         string
+	Description  string
+	IngressRules []IngressRules
+	VPC          *ec2.Vpc
+}
+
+type SGResources struct {
+	SG *ec2.SecurityGroup
+}
+
+func (r SGRequest) Create(ctx *pulumi.Context) (*SGResources, error) {
+	sgName := fmt.Sprintf("%s-%s", "sg", r.Name)
+	sg, err := ec2.NewSecurityGroup(ctx,
+		sgName,
+		&ec2.SecurityGroupArgs{
+			Description: pulumi.String(r.Description),
+			VpcId:       r.VPC.ID(),
+			Ingress:     getSecurityGroupIngressArray(r.IngressRules),
+			Egress:      ec2.SecurityGroupEgressArray{egressAll},
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String(sgName),
+			},
+		})
+	if err != nil {
+		return nil, err
+	}
+	return &SGResources{SG: sg}, nil
+}
+
+func getSecurityGroupIngressArray(rules []IngressRules) (sgia ec2.SecurityGroupIngressArray) {
+	for _, r := range rules {
+		sgia = append(sgia, &ec2.SecurityGroupIngressArgs{
+			Description: pulumi.String(r.Description),
+			FromPort:    pulumi.Int(r.FromPort),
+			ToPort:      pulumi.Int(r.ToPort),
+			Protocol:    pulumi.String(r.Protocol),
+			CidrBlocks: pulumi.StringArray{
+				pulumi.String(infra.NETWORKING_CIDR_ANY_IPV4),
+			},
+			Ipv6CidrBlocks: pulumi.StringArray{
+				pulumi.String(infra.NETWORKING_CIDR_ANY_IPV6),
+			},
+		})
+	}
+	return nil
+}
