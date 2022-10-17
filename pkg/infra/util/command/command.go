ef4a697f076d9aaf9d2c0cd75da0fa0b1e6ad7c3
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-10-17 11:28:40 +0200

    fix command wait for init on bastion

--- pkg/infra/util/command/command.go
+++ pkg/infra/util/command/command.go
@@ -1,9 +1,6 @@
 package command
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
 	"github.com/pulumi/pulumi-command/sdk/go/command/remote"
 	"github.com/pulumi/pulumi-tls/sdk/v4/go/tls"
@@ -11,9 +8,9 @@ import (
 )
 
 const (
-	remoteCommandTimeout int = 300
+	// remoteCommandTimeout int = 300
 	// https://www.pulumi.com/docs/intro/concepts/resources/options/customtimeouts/
-	// remoteCommandTimeout string = "5m"
+	remoteTimeout string = "10m"
 
 	CommandCloudInitWait string = "sudo cloud-init status --wait"
 	CommandPing          string = "echo ping"
@@ -39,27 +36,30 @@ func (r RemoteInstance) RemoteCommand(ctx *pulumi.Context, remoteCommand string)
 			Port:       pulumi.Float64(defaultSSHPort),
 		},
 		Create: pulumi.String(remoteCommand),
-		Update: pulumi.String(remoteCommand)})
-	// }, pulumi.Timeouts(&pulumi.CustomTimeouts{Create: remoteCommandTimeout}))
+		Update: pulumi.String(remoteCommand),
+	}, pulumi.Timeouts(
+		&pulumi.CustomTimeouts{
+			Create: remoteTimeout,
+			Update: remoteTimeout}))
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (r RemoteInstance) RemoteCommandAwait(c RemoteCommand, ctx *pulumi.Context, remoteCommand string) error {
-	// this is to avoid garbage leak using direclty ticker
-	ticker := time.NewTicker(500 * time.Millisecond)
-	defer func() { ticker.Stop() }()
-	timer := time.After(time.Duration(remoteCommandTimeout) * time.Second)
-	for {
-		select {
-		case <-ticker.C:
-			if err := c(ctx, remoteCommand); err == nil {
-				return nil
-			}
-		case <-timer:
-			return fmt.Errorf("command %s failed", remoteCommand)
-		}
-	}
-}
+// func (r RemoteInstance) RemoteCommandAwait(c RemoteCommand, ctx *pulumi.Context, remoteCommand string) error {
+// 	// this is to avoid garbage leak using direclty ticker
+// 	ticker := time.NewTicker(500 * time.Millisecond)
+// 	defer func() { ticker.Stop() }()
+// 	timer := time.After(time.Duration(remoteCommandTimeout) * time.Second)
+// 	for {
+// 		select {
+// 		case <-ticker.C:
+// 			if err := c(ctx, remoteCommand); err == nil {
+// 				return nil
+// 			}
+// 		case <-timer:
+// 			return fmt.Errorf("command %s failed", remoteCommand)
+// 		}
+// 	}
+// }
