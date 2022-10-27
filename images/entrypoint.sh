3de43b9e9e37acfd5c8b885b6898ef641db0c3ee
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-10-27 12:47:41 +0200

    container image to create or destroy supported hosts

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -2,14 +2,19 @@
 
 VALID_CONFIG=true
 # Check required ENVs
-if [ -z "${INSTANCE_TYPES}" ]; then 
-  echo "INSTANCE_TYPES is required"
+if [ -z "${OPERATION}" ]; then 
+  echo "OPERATION is required"
   VALID_CONFIG=false
 fi
 
-if [ -z "${PRODUCT_DESCRIPTION}" ]; then 
-  echo "PRODUCT_DESCRIPTION ENV is required"
+if [ -z "${PROJECT_NAME}" ]; then 
+  echo "PROJECT_NAME ENV is required"
   VALID_CONFIG=false  
+fi
+
+if [ -z "${BACKED_URL}" ]; then 
+  echo "${DEFAULT_BACKED_URL} will be used as backed url it will be exported as volume"
+  BACKED_URL="${DEFAULT_BACKED_URL}"
 fi
 
 if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ] || [ -z "${AWS_DEFAULT_REGION}" ]; then 
@@ -27,7 +32,25 @@ if [ "${VALID_CONFIG}" = false ]; then
   exit 1
 fi
 
-# Run qenvs
-exec qenvs spot \
-    --instance-types "${INSTANCE_TYPES}" \
-    --product-description "${PRODUCT_DESCRIPTION}"
+# //https://docs.aws.amazon.com/sdk-for-go/api/aws/session/
+AWS_SDK_LOAD_CONFIG=1
+# https://www.pulumi.com/docs/reference/cli/environment-variables/
+PULUMI_CONFIG_PASSPHRASE="passphrase"
+
+if [[ "${OPERATION}" == "create" ]]; then
+    if [ -z "${SUPPORTED_HOST_ID}" ]; then 
+      echo "SUPPORTED_HOST_ID is required"
+      VALID_CONFIG=false
+    fi
+    exec qenvs host create \
+      --project-name "${PROJECT_NAME}" \
+      --backed-url "${BACKED_URL}" \
+      --host-id "${SUPPORTED_HOST_ID}"
+else 
+  exec qenvs host destroy \
+      --project-name "${PROJECT_NAME}" \
+      --backed-url "${BACKED_URL}" 
+fi
+
+
+
