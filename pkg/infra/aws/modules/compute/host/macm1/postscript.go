b9830b72ecb0e9b2945fc49d97f417aef1453cb1
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-12-14 14:03:00 +0100

    fix password special characs for macm1 on postscript
    password should be included on quotes to avoid wrong interpretation of special chars inside it

--- pkg/infra/aws/modules/compute/host/macm1/postscript.go
+++ pkg/infra/aws/modules/compute/host/macm1/postscript.go
@@ -11,12 +11,12 @@ sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.app
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
 
 # Set user password
-sudo dscl . -passwd /Users/{{.Username}} {{.Password}}
+sudo dscl . -passwd /Users/{{.Username}} "{{.Password}}"
 
 # Autologin
 sudo curl -o /tmp/kcpassword https://raw.githubusercontent.com/xfreebird/kcpassword/master/kcpassword
 sudo chmod +x /tmp/kcpassword
-sudo /tmp/kcpassword {{.Password}}
+sudo /tmp/kcpassword "{{.Password}}"
 sudo defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser "{{.Username}}"
 
 sudo defaults write /Library/Preferences/.GlobalPreferences.plist com.apple.securitypref.logoutvalue -int 0
