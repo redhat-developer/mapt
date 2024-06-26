448ec6dddcec4d6371dd7a2b5fa82af7a374a72d
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2024-06-26 15:22:42 +0200

    fix: windows aws machine force reboot after bootstrap script. To ensure ssh service is properly setup Fix #236
    Signed-off-by: Adrian Riobo Lorenzo <ariobolo@redhat.com>

--- pkg/provider/aws/action/windows/bootstrap.ps1
+++ pkg/provider/aws/action/windows/bootstrap.ps1
@@ -19,7 +19,8 @@ Invoke-Command -ScriptBlock { New-Item -Path C:\Users\{{.Username}}\.ssh -Name "
 # Set back disable unnecrypted communications
 Set-Item .\allowunencrypted $false
 
-# Restart computer to have the ssh connection available with setup from this script
-# Start-Process powershell -verb runas -ArgumentList "Restart-Computer -Force"
 netsh advfirewall firewall add rule name="Open SSH Port 22" dir=in action=allow protocol=TCP localport=22 remoteip=any
+# Restart computer to have the ssh connection available with setup from this script
+Start-Process powershell -verb runas -ArgumentList "Restart-Computer -Force"
+
 </powershell>
