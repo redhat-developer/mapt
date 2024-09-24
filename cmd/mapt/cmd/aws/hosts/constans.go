17fac09a23fe065a8ad29d5ed6a6a338d6d333dc
Anjan Nath <kaludios@gmail.com>
2024-09-24 13:19:42 +0530

    update vm-size and vm-types flag description
    the default vm-size for azure and the default vm-type for
    aws is decided based on the cpus, memory and arch flags
    
    if one of vm-types and vm-sizes is set then those values
    are ignored

--- cmd/mapt/cmd/aws/hosts/constans.go
+++ cmd/mapt/cmd/aws/hosts/constans.go
@@ -7,5 +7,5 @@ const (
 	airgapDesc string = "if this flag is set the host will be created as airgap machine. Access will done through a bastion"
 
 	vmTypes            string = "vm-types"
-	vmTypesDescription string = "set an specific set of vm-types. Note vm-type should match requested arch. Also if --spot flag is used set at least 3 types."
+	vmTypesDescription string = "set an specific set of vm-types and ignore any CPUs, Memory, Arch parameters set. Note vm-type should match requested arch. Also if --spot flag is used set at least 3 types."
 )
