84db51d00d5c3d1b9875848a37319514c5a8127d
Anjan Nath <kaludios@gmail.com>
2024-12-16 20:06:02 +0530

    check if best spot price location supports resource group
    the location suggested by the spot calculation sometimes do not
    support the creation of Resource Groups  which is a requirement
    for `mapt`, In those cases we just use a default region
    
    we get the following error when a region is not usable:
    
    ```
    Diagnostics:
      azure-native:resources:ResourceGroup (az-ghrunner-awd-rg):
        error: autorest/azure: Service returned an error. Status=400 Code="LocationNotAvailableForResourceGroup" [...]
    ```

--- pkg/provider/azure/azure.go
+++ pkg/provider/azure/azure.go
@@ -1,6 +1,8 @@
 package azure
 
 import (
+	"slices"
+
 	"github.com/redhat-developer/mapt/pkg/manager"
 	"github.com/redhat-developer/mapt/pkg/manager/credentials"
 )
@@ -11,7 +13,55 @@ func GetClouProviderCredentials(fixedCredentials map[string]string) credentials.
 		FixedCredentials:  fixedCredentials}
 }
 
-var DefaultCredentials = GetClouProviderCredentials(nil)
+var (
+	DefaultCredentials               = GetClouProviderCredentials(nil)
+	locationsSupportingResourceGroup = []string{
+		"eastasia",
+		"southeastasia",
+		"australiaeast",
+		"australiasoutheast",
+		"brazilsouth",
+		"canadacentral",
+		"canadaeast",
+		"switzerlandnorth",
+		"germanywestcentral",
+		"eastus2",
+		"eastus",
+		"centralus",
+		"northcentralus",
+		"francecentral",
+		"uksouth",
+		"ukwest",
+		"centralindia",
+		"southindia",
+		"jioindiawest",
+		"italynorth",
+		"japaneast",
+		"japanwest",
+		"koreacentral",
+		"koreasouth",
+		"mexicocentral",
+		"northeurope",
+		"norwayeast",
+		"polandcentral",
+		"qatarcentral",
+		"spaincentral",
+		"swedencentral",
+		"uaenorth",
+		"westcentralus",
+		"westeurope",
+		"westus2",
+		"westus",
+		"southcentralus",
+		"westus3",
+		"southafricanorth",
+		"australiacentral",
+		"australiacentral2",
+		"israelcentral",
+		"westindia",
+		"newzealandnorth",
+	}
+)
 
 func Destroy(projectName, backedURL, stackName string) error {
 	stack := manager.Stack{
@@ -20,4 +70,16 @@ func Destroy(projectName, backedURL, stackName string) error {
 		BackedURL:           backedURL,
 		ProviderCredentials: DefaultCredentials}
 	return manager.DestroyStack(stack)
+}
+
+func locationSupportsResourceGroup(location string) bool {
+	return slices.Contains(locationsSupportingResourceGroup, location)
+}
+
+func GetSuitableLocationForResourceGroup(location string) string {
+	if locationSupportsResourceGroup(location) {
+		return location
+	}
+
+	return locationsSupportingResourceGroup[0]
 }
