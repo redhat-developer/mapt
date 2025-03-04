daec224ce47197d0e2eab4caec022876d55b38b0
Anjan Nath <kaludios@gmail.com>
2025-03-04 19:17:16 +0530

    fix parameters for ghactions runner config struct

--- cmd/mapt/cmd/azure/hosts/windows.go
+++ cmd/mapt/cmd/azure/hosts/windows.go
@@ -92,8 +92,8 @@ func getCreateWindowsDesktop() *cobra.Command {
 			if viper.IsSet(params.InstallGHActionsRunner) {
 				ctx.GHRunnerArgs = &github.GithubRunnerArgs{
 					Token:   viper.GetString(params.GHActionsRunnerToken),
-					RepoURL: viper.GetString(params.GHActionsRunnerName),
-					Name:    viper.GetString(params.GHActionsRunnerRepo),
+					RepoURL: viper.GetString(params.GHActionsRunnerRepo),
+					Name:    viper.GetString(params.GHActionsRunnerName),
 					Labels:  viper.GetStringSlice(params.GHActionsRunnerLabels)}
 			}
 
