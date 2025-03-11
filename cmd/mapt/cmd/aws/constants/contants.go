9789ae2af59c7db40cac24f9d836aaefd012414d
Anjan Nath <kaludios@gmail.com>
2025-03-11 16:24:34 +0530

    cmd: fix macos default version typo for mac-pool service

--- cmd/mapt/cmd/aws/constants/contants.go
+++ cmd/mapt/cmd/aws/constants/contants.go
@@ -15,7 +15,7 @@ const (
 	MACArchDesc          string = "MAC architecture allowed values x86, m1, m2"
 	MACArchDefault       string = mac.DefaultArch
 	MACOSVersion         string = "version"
-	MACOSVersionDesc     string = "MACos operating system vestion 11, 12 on x86 and m1/m2; 13, 14 on all archs"
+	MACOSVersionDesc     string = "MACos operating system version 11, 12 on x86 and m1/m2; 13, 14, 15 on all archs"
 	MACOSVersionDefault  string = mac.DefaultOSVersion
 	MACFixedLocation     string = "fixed-location"
 	MACFixedLocationDesc string = "if this flag is set the host will be created only on the region set by the AWS Env (AWS_DEFAULT_REGION)"
