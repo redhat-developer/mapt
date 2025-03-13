dd9e11bbd8b4f6810f43384d846ea9ab0677e6a1
Adrian Riobo <ariobolo@redhat.com>
2025-03-13 17:55:24 +0100

    chore: aws added tags to fargate task spec
    Signed-off-by: Adrian Riobo <ariobolo@redhat.com>

---
+++ pkg/provider/aws/modules/mac/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	TagKeyPrefix    = "prefix"
+	TagKeyBackedURL = "backedURL"
+	TagKeyArch      = "arch"
+	// tags added when dedicated host is part of a pool
+	TagKeyOSVersion = "osVersion"
+	TagKeyPoolName  = "poolName"
+)
