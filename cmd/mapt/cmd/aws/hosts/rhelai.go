849fc4c16c289d5f6ea3c5482f7f8132b7ae0a1a
Adrian Riobo <ariobolo@redhat.com>
2025-07-11 10:59:22 +0200

    feat: added rhel-ai support on aws
    Signed-off-by: Adrian Riobo <ariobolo@redhat.com>

---
+++ cmd/mapt/cmd/aws/hosts/rhelai.go
@@ -0,0 +1,114 @@
+package hosts
+
+import (
+	awsParams "github.com/redhat-developer/mapt/cmd/mapt/cmd/aws/constants"
+	"github.com/redhat-developer/mapt/cmd/mapt/cmd/params"
+	maptContext "github.com/redhat-developer/mapt/pkg/manager/context"
+	rhelai "github.com/redhat-developer/mapt/pkg/provider/aws/action/rhel-ai"
+	"github.com/redhat-developer/mapt/pkg/util/logging"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const (
+	cmdRHELAI     = "rhel-ai"
+	cmdRHELAIDesc = "manage rhel ai host"
+)
+
+func GetRHELAICmd() *cobra.Command {
+	c := &cobra.Command{
+		Use:   cmdRHELAI,
+		Short: cmdRHELAIDesc,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := viper.BindPFlags(cmd.Flags()); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+
+	flagSet := pflag.NewFlagSet(cmdRHELAI, pflag.ExitOnError)
+	params.AddCommonFlags(flagSet)
+	c.PersistentFlags().AddFlagSet(flagSet)
+
+	c.AddCommand(getRHELAICreate(), getRHELAIDestroy())
+	return c
+}
+
+func getRHELAICreate() *cobra.Command {
+	c := &cobra.Command{
+		Use:   params.CreateCmdName,
+		Short: params.CreateCmdName,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := viper.BindPFlags(cmd.Flags()); err != nil {
+				return err
+			}
+
+			ctx := &maptContext.ContextArgs{
+				ProjectName:           viper.GetString(params.ProjectName),
+				BackedURL:             viper.GetString(params.BackedURL),
+				ResultsOutput:         viper.GetString(params.ConnectionDetailsOutput),
+				Debug:                 viper.IsSet(params.Debug),
+				DebugLevel:            viper.GetUint(params.DebugLevel),
+				SpotPriceIncreaseRate: viper.GetInt(params.SpotPriceIncreaseRate),
+				Tags:                  viper.GetStringMapString(params.Tags),
+			}
+
+			// Run create
+			if err := rhelai.Create(
+				ctx,
+				&rhelai.RHELAIArgs{
+					Prefix:         "main",
+					Version:        viper.GetString(params.RhelAIVersion),
+					SubsUsername:   viper.GetString(params.SubsUsername),
+					SubsUserpass:   viper.GetString(params.SubsUserpass),
+					ComputeRequest: params.GetComputeRequest(),
+					Spot:           viper.IsSet(awsParams.Spot),
+					Timeout:        viper.GetString(params.Timeout),
+				}); err != nil {
+				logging.Error(err)
+			}
+			return nil
+		},
+	}
+	flagSet := pflag.NewFlagSet(params.CreateCmdName, pflag.ExitOnError)
+	flagSet.StringP(params.ConnectionDetailsOutput, "", "", params.ConnectionDetailsOutputDesc)
+	flagSet.StringToStringP(params.Tags, "", nil, params.TagsDesc)
+	flagSet.StringP(params.RhelAIVersion, "", params.RhelAIVersionDefault, params.RhelAIVersionDesc)
+	flagSet.StringP(params.SubsUsername, "", "", params.SubsUsernameDesc)
+	flagSet.StringP(params.SubsUserpass, "", "", params.SubsUserpassDesc)
+	flagSet.Bool(awsParams.Spot, false, awsParams.SpotDesc)
+	flagSet.IntP(params.SpotPriceIncreaseRate, "", params.SpotPriceIncreaseRateDefault, params.SpotPriceIncreaseRateDesc)
+	flagSet.StringP(params.Timeout, "", "", params.TimeoutDesc)
+	flagSet.AddFlagSet(params.GetCpusAndMemoryFlagset())
+	c.PersistentFlags().AddFlagSet(flagSet)
+	return c
+}
+
+func getRHELAIDestroy() *cobra.Command {
+	c := &cobra.Command{
+		Use:   params.DestroyCmdName,
+		Short: params.DestroyCmdName,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := viper.BindPFlags(cmd.Flags()); err != nil {
+				return err
+			}
+
+			if err := rhelai.Destroy(&maptContext.ContextArgs{
+				ProjectName: viper.GetString(params.ProjectName),
+				BackedURL:   viper.GetString(params.BackedURL),
+				Debug:       viper.IsSet(params.Debug),
+				DebugLevel:  viper.GetUint(params.DebugLevel),
+				Serverless:  viper.IsSet(params.Serverless),
+			}); err != nil {
+				logging.Error(err)
+			}
+			return nil
+		},
+	}
+	flagSet := pflag.NewFlagSet(params.DestroyCmdName, pflag.ExitOnError)
+	flagSet.Bool(params.Serverless, false, params.ServerlessDesc)
+	c.PersistentFlags().AddFlagSet(flagSet)
+	return c
+}
