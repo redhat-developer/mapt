28faf3124cfd031285ef05b6e459b3b3362011fd
Adrian Riobo <ariobolo@redhat.com>
2026-02-17 15:03:35 +0100

    chore: openshift-snc with disable-cluster-readiness flag export kubeconfig
    Previously we introduced the disable-cluster-readiness to allow early access to the cluster, within that commit we hide the kubeconfig export when the flag was enable, actually it can be exported no matter the value of the flag. This commit enables it. Fixes #719.
    
    Signed-off-by: Adrian Riobo <ariobolo@redhat.com>

---
+++ pkg/provider/aws/action/snc/constants.go
@@ -0,0 +1,22 @@
+package openshiftsnc
+
+import "fmt"
+
+var (
+	diskSize int = 200
+
+	// This is managed by https://github.com/devtools-qe-incubator/cloud-importer
+	amiProduct = "Linux/UNIX"
+	// amiProductDescription = "Red Hat Enterprise Linux"
+	amiRegex       = "openshift-local-%s-%s-*"
+	amiUserDefault = "core"
+	amiOwner       = "391597328979"
+	// amiOriginRegion       = "us-east-1"
+
+	// SSM
+	ocpPullSecretID = "ocppullsecretid"
+	kapass          = "kapass"
+	devpass         = "devpass"
+)
+
+func amiName(version, arch *string) string { return fmt.Sprintf(amiRegex, *version, *arch) }
