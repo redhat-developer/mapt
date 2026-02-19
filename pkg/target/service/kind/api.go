7cfd4efe8d6f84783d1473c8a07d083ad6178def
Adrian Riobo <ariobolo@redhat.com>
2026-02-19 12:08:16 +0100

    fix(kind): Update kind related dependecies
    bump kind version to v0.31.0 and fedora to 43
    
    Signed-off-by: Adrian Riobo <ariobolo@redhat.com>

--- pkg/target/service/kind/api.go
+++ pkg/target/service/kind/api.go
@@ -12,10 +12,11 @@ type KindK8SImages struct {
 }
 
 var KindK8sVersions map[string]KindK8SImages = map[string]KindK8SImages{
-	"v1.34": {"v0.30.0", "kindest/node:v1.34.0@sha256:7416a61b42b1662ca6ca89f02028ac133a309a2a30ba309614e8ec94d976dc5a"},
-	"v1.33": {"v0.30.0", "kindest/node:v1.33.4@sha256:25a6018e48dfcaee478f4a59af81157a437f15e6e140bf103f85a2e7cd0cbbf2"},
-	"v1.32": {"v0.30.0", "kindest/node:v1.32.8@sha256:abd489f042d2b644e2d033f5c2d900bc707798d075e8186cb65e3f1367a9d5a1"},
-	"v1.31": {"v0.30.0", "kindest/node:v1.31.12@sha256:0f5cc49c5e73c0c2bb6e2df56e7df189240d83cf94edfa30946482eb08ec57d2"},
+	"v1.35": {"v0.31.0", "kindest/node:v1.35.0@sha256:452d707d4862f52530247495d180205e029056831160e22870e37e3f6c1ac31f"},
+	"v1.34": {"v0.31.0", "kindest/node:v1.34.3@sha256:08497ee19eace7b4b5348db5c6a1591d7752b164530a36f855cb0f2bdcbadd48"},
+	"v1.33": {"v0.31.0", "kindest/node:v1.33.7@sha256:d26ef333bdb2cbe9862a0f7c3803ecc7b4303d8cea8e814b481b09949d353040"},
+	"v1.32": {"v0.31.0", "kindest/node:v1.32.11@sha256:5fc52d52a7b9574015299724bd68f183702956aa4a2116ae75a63cb574b35af8"},
+	"v1.31": {"v0.31.0", "kindest/node:v1.31.14@sha256:6f86cf509dbb42767b6e79debc3f2c32e4ee01386f0489b3b2be24b0a55aac2b"},
 	"v1.30": {"v0.29.0", "kindest/node:v1.30.13@sha256:397209b3d947d154f6641f2d0ce8d473732bd91c87d9575ade99049aa33cd648"},
 }
 
