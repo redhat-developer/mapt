22cb4d59efdb2940819dd4b3d701e1391255e310
Adrian Riobo <ariobolo@redhat.com>
2026-09-07 17:32:15 +0200

    fix(deps): update all dependencies

--- tools/vendor/github.com/mgechev/revive/rule/utils.go
+++ tools/vendor/github.com/mgechev/revive/rule/utils.go
@@ -1,42 +1,13 @@
 package rule
 
 import (
-	"go/ast"
 	"go/token"
 	"regexp"
 	"strings"
 
-	"github.com/mgechev/revive/internal/astutils"
+	"github.com/mgechev/revive/internal/config"
 	"github.com/mgechev/revive/lint"
 )
-
-// exitFuncChecker is a function type that checks whether a function call is an exit function.
-type exitFuncChecker func(args []ast.Expr) bool
-
-var alwaysTrue exitFuncChecker = func([]ast.Expr) bool { return true }
-
-// exitFunctions is a map of std packages and functions that are considered as exit functions.
-var exitFunctions = map[string]map[string]exitFuncChecker{
-	"os":      {"Exit": alwaysTrue},
-	"syscall": {"Exit": alwaysTrue},
-	"log": {
-		"Fatal":   alwaysTrue,
-		"Fatalf":  alwaysTrue,
-		"Fatalln": alwaysTrue,
-		"Panic":   alwaysTrue,
-		"Panicf":  alwaysTrue,
-		"Panicln": alwaysTrue,
-	},
-	"flag": {
-		"Parse": func([]ast.Expr) bool { return true },
-		"NewFlagSet": func(args []ast.Expr) bool {
-			if len(args) != 2 {
-				return false
-			}
-			return astutils.IsPkgDotName(args[1], "flag", "ExitOnError")
-		},
-	},
-}
 
 func srcLine(src []byte, p token.Position) string {
 	// Run to end of line in both directions if not at line start/end.
@@ -52,14 +23,7 @@ func srcLine(src []byte, p token.Position) string {
 
 // isRuleOption returns true if arg and name are the same after normalization.
 func isRuleOption(arg, name string) bool {
-	return normalizeRuleOption(arg) == normalizeRuleOption(name)
-}
-
-// normalizeRuleOption returns an option name from the argument. It is lowercased and without hyphens.
-//
-// Example: normalizeRuleOption("allowTypesBefore"), normalizeRuleOption("allow-types-before") -> "allowtypesbefore".
-func normalizeRuleOption(arg string) string {
-	return strings.ToLower(strings.ReplaceAll(arg, "-", ""))
+	return config.NormalizeOption(arg) == config.NormalizeOption(name)
 }
 
 var normalizePathReplacer = strings.NewReplacer("-", "", "_", "", ".", "")
@@ -90,21 +54,6 @@ var directiveCommentRE = regexp.MustCompile("^//(line |extern |export |[a-z0-9]+
 
 func isDirectiveComment(line string) bool {
 	return directiveCommentRE.MatchString(line)
-}
-
-// isCallToExitFunction checks if the function call is a call to an exit function.
-func isCallToExitFunction(pkgName, functionName string, callArgs []ast.Expr) bool {
-	m, ok := exitFunctions[pkgName]
-	if !ok {
-		return false
-	}
-
-	check, ok := m[functionName]
-	if !ok {
-		return false
-	}
-
-	return check(callArgs)
 }
 
 // newInternalFailureError returns a slice of Failure with a single internal failure in it.
